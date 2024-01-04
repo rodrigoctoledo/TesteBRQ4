57b98fe95f8af003fec0c800965abbdd1026ce96
rodrigoctoledo <rodrigo_toledo@outlook.com>
2024-01-04 17:08:33 -0300

    Add files via upload

---
+++ teste/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConverter = new System.Windows.Forms.Button();
+            this.textBoxNumeros = new System.Windows.Forms.TextBox();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConverter
+            // 
+            this.buttonConverter.Location = new System.Drawing.Point(154, 89);
+            this.buttonConverter.Name = "buttonConverter";
+            this.buttonConverter.Size = new System.Drawing.Size(75, 23);
+            this.buttonConverter.TabIndex = 0;
+            this.buttonConverter.Text = "Calc";
+            this.buttonConverter.UseVisualStyleBackColor = true;
+            this.buttonConverter.Click += new System.EventHandler(this.buttonConverter_Click);
+            // 
+            // textBoxNumeros
+            // 
+            this.textBoxNumeros.Location = new System.Drawing.Point(33, 89);
+            this.textBoxNumeros.Name = "textBoxNumeros";
+            this.textBoxNumeros.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNumeros.TabIndex = 1;
+            // 
+            // textBoxResultado
+            // 
+            this.textBoxResultado.Location = new System.Drawing.Point(22, 242);
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.Size = new System.Drawing.Size(455, 22);
+            this.textBoxResultado.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxResultado);
+            this.Controls.Add(this.textBoxNumeros);
+            this.Controls.Add(this.buttonConverter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConverter;
+        private System.Windows.Forms.TextBox textBoxNumeros;
+        private System.Windows.Forms.TextBox textBoxResultado;
+    }
+}
+
---
+++ teste/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       private void buttonConverter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtenha a lista de números a partir da caixa de texto
+                string[] numerosString = textBoxNumeros.Text.Split(',');
+                List<int> numeros = new List<int>();
+
+                foreach (var numeroString in numerosString)
+                {
+                    if (int.TryParse(numeroString, out int numero))
+                    {
+                        numeros.Add(numero);
+                    }
+                    else
+                    {
+                        throw new FormatException("Formato inválido para os números.");
+                    }
+                }
+
+                // Converta os números para as palavras "POSITIVO" ou "NEGATIVO"
+                List<string> numerosConvertidos = numeros.Select(num => num >= 0 ? "POSITIVO" : "NEGATIVO").ToList();
+
+                // Exiba o resultado na caixa de texto de saída
+                textBoxResultado.Text = string.Join(", ", numerosConvertidos);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
